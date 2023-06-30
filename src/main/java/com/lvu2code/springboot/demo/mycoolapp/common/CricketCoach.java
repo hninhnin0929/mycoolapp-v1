df6d7a81a1ff89ddb58f0501143101f61a119d71
hninhnin0929 <hninhnin0929@gmail.com>
2023-06-30 10:47:43 +0900

    prototype scope

--- src/main/java/com/lvu2code/springboot/demo/mycoolapp/common/CricketCoach.java
+++ src/main/java/com/lvu2code/springboot/demo/mycoolapp/common/CricketCoach.java
@@ -1,8 +1,11 @@
 package com.lvu2code.springboot.demo.mycoolapp.common;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component  // @Component annotation marks the class as a Spring bean
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class CricketCoach implements Coach {
 
     public CricketCoach(){
