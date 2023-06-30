35c9e43a78991c708b5bb923633e2cd4b220baa9
hninhnin0929 <hninhnin0929@gmail.com>
2023-06-30 13:07:06 +0900

    custom bean id

--- src/main/java/com/lvu2code/springboot/demo/mycoolapp/rest/DemoController.java
+++ src/main/java/com/lvu2code/springboot/demo/mycoolapp/rest/DemoController.java
@@ -24,7 +24,7 @@ public class DemoController {
 //    }
     @Autowired
     public DemoController(
-            @Qualifier("swimCoach") Coach theCoach){
+            @Qualifier("aquatic") Coach theCoach){
 
         System.out.println("In constructor: " + getClass().getSimpleName());
 
