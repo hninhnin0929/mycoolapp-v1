ec1e05eb432b864d51a5b3f3ddb650105122c982
hninhnin0929 <hninhnin0929@gmail.com>
2023-06-30 10:39:40 +0900

    testing default bean scope

--- src/main/java/com/lvu2code/springboot/demo/mycoolapp/rest/DemoController.java
+++ src/main/java/com/lvu2code/springboot/demo/mycoolapp/rest/DemoController.java
@@ -11,6 +11,7 @@ public class DemoController {
 
     // define a private field for the dependency
     private Coach myCoach;
+    private Coach anotherCoach;
 
     // define a constructor for dependency injection
 //    @Autowired  //@Autowired annotation tells Spring to inject a dependency
@@ -23,16 +24,23 @@ public class DemoController {
 //        myCoach = theCoach;
 //    }
     @Autowired
-    public DemoController(@Qualifier("cricketCoach") Coach theCoach){
+    public DemoController(
+            @Qualifier("cricketCoach") Coach theCoach,
+            @Qualifier("cricketCoach") Coach theAnotherCoach){
 
         System.out.println("In constructor: " + getClass().getSimpleName());
 
         myCoach = theCoach;
+        anotherCoach = theAnotherCoach;
     }
-
 
     @GetMapping("/dailyworkout")
     public String getDailyWorkout(){
         return myCoach.getDailyWorkout();
+    }
+
+    @GetMapping("/check")
+    public String check() {
+        return "Comparing beans: myCoach === anotherCoach, " + (myCoach == anotherCoach);
     }
 }
