43260ae478d244aaa19390f05177176ff6eab85d
hninhnin0929 <hninhnin0929@gmail.com>
2023-07-03 09:48:14 +0900

    turn off spring boot banner and reduce logging level

--- src/main/java/com/lvu2code/springboot/demo/mycoolapp/MycoolappApplication.java
+++ src/main/java/com/lvu2code/springboot/demo/mycoolapp/MycoolappApplication.java
@@ -1,7 +1,9 @@
 package com.lvu2code.springboot.demo.mycoolapp;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 //@SpringBootApplication(
 //		scanBasePackages = {
@@ -16,4 +18,12 @@ public class MycoolappApplication {
 		SpringApplication.run(MycoolappApplication.class, args);
 	}
 
+	// From the Spring Boot Framework
+	// Executed after the Spring Beans have been loaded
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args){
+		return runner -> {
+			System.out.println("Hellow World");
+		};
+	}
 }
