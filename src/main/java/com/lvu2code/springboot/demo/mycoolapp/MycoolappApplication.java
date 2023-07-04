b3a351b50b571257dca76bfb76374c65618111da
hninhnin0929 <hninhnin0929@gmail.com>
2023-07-04 13:07:37 +0900

    delete all

--- src/main/java/com/lvu2code/springboot/demo/mycoolapp/MycoolappApplication.java
+++ src/main/java/com/lvu2code/springboot/demo/mycoolapp/MycoolappApplication.java
@@ -41,8 +41,17 @@ public class MycoolappApplication {
 
 //			updateStudent(studentDAO);
 
-			deleteStudent(studentDAO);
+//			deleteStudent(studentDAO);
+
+			deleteAllStudents(studentDAO);
 		};
+	}
+
+	private void deleteAllStudents(StudentDAO studentDAO) {
+
+		System.out.println("Deleting all students");
+		int numRowsDeleted = studentDAO.deleteAll();
+		System.out.println("Deleted row count: " + numRowsDeleted);
 	}
 
 	private void deleteStudent(StudentDAO studentDAO) {
