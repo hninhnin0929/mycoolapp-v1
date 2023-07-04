cca0ff7126d8fdac17286492f299965f0b6b2e3e
hninhnin0929 <hninhnin0929@gmail.com>
2023-07-04 11:31:47 +0900

    updating student by id

--- src/main/java/com/lvu2code/springboot/demo/mycoolapp/MycoolappApplication.java
+++ src/main/java/com/lvu2code/springboot/demo/mycoolapp/MycoolappApplication.java
@@ -37,8 +37,28 @@ public class MycoolappApplication {
 			
 //			queryForStudents(studentDAO);
 			
-			queryForStudentsByLastName(studentDAO);
+//			queryForStudentsByLastName(studentDAO);
+
+			updateStudent(studentDAO);
 		};
+	}
+
+	private void updateStudent(StudentDAO studentDAO) {
+
+		// retrieve student based on the id: primary key
+		int studentId = 1;
+		System.out.println("Getting student with id: " + studentId);
+		Student myStudent = studentDAO.findById(studentId);
+
+		// change first name to "Scooby"
+		System.out.println("Updating student ...");
+		myStudent.setFirstName("Scooby");
+
+		// update the student
+		studentDAO.update(myStudent);
+
+		// display the updated student
+		System.out.println("Updated student: " + myStudent);
 	}
 
 	private void queryForStudentsByLastName(StudentDAO studentDAO) {
