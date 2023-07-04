2604a20136284bea538bc0830a33e3cdcb6ecf82
hninhnin0929 <hninhnin0929@gmail.com>
2023-07-04 11:19:19 +0900

    queryForStudentsByLastName

--- src/main/java/com/lvu2code/springboot/demo/mycoolapp/MycoolappApplication.java
+++ src/main/java/com/lvu2code/springboot/demo/mycoolapp/MycoolappApplication.java
@@ -35,8 +35,21 @@ public class MycoolappApplication {
 			
 //			readStudent(studentDAO);
 			
-			queryForStudents(studentDAO);
+//			queryForStudents(studentDAO);
+			
+			queryForStudentsByLastName(studentDAO);
 		};
+	}
+
+	private void queryForStudentsByLastName(StudentDAO studentDAO) {
+
+		// get a list of students
+		List<Student> theStudents = studentDAO.findByLastName("Duck");
+
+		// display list of students
+		for (Student tempStudent : theStudents) {
+			System.out.println(tempStudent);
+		}
 	}
 
 	private void queryForStudents(StudentDAO studentDAO) {
