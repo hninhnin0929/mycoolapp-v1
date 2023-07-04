aee71c3cd111618574b62889063bc76c6f398e4a
hninhnin0929 <hninhnin0929@gmail.com>
2023-07-04 10:08:01 +0900

    save students

--- src/main/java/com/lvu2code/springboot/demo/mycoolapp/MycoolappApplication.java
+++ src/main/java/com/lvu2code/springboot/demo/mycoolapp/MycoolappApplication.java
@@ -26,9 +26,25 @@ public class MycoolappApplication {
 	public CommandLineRunner commandLineRunner(StudentDAO studentDAO){
 		return runner -> {
 			System.out.println("Hello world");
-			createStudent(studentDAO);
-
+//			createStudent(studentDAO);
+			createMultipleStudents(studentDAO);
 		};
+	}
+
+	private void createMultipleStudents(StudentDAO studentDAO) {
+
+		// create multiple students
+		System.out.println("Creating 3 student objects ...");
+		Student tempStudent1 = new Student("John", "Doe", "john@luv2code.com");
+		Student tempStudent2 = new Student("Mary", "Public", "mary@luv2code.com");
+		Student tempStudent3 = new Student("Bonita", "Applebum", "bonita@luv2code.com");
+
+		// save the sutdent objects
+		System.out.println("Saving the students ...");
+		studentDAO.save(tempStudent1);
+		studentDAO.save(tempStudent2);
+		studentDAO.save(tempStudent3);
+
 	}
 
 	private void createStudent(StudentDAO studentDAO) {
