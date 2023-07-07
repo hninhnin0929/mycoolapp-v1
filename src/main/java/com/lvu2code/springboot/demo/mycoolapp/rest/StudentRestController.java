701ed0fd069d970cfc79fa98411cfab66b1f8f88
hninhnin0929 <hninhnin0929@gmail.com>
2023-07-07 15:27:40 +0900

    @ControllerAdvice for exception handling best practise

--- src/main/java/com/lvu2code/springboot/demo/mycoolapp/rest/StudentRestController.java
+++ src/main/java/com/lvu2code/springboot/demo/mycoolapp/rest/StudentRestController.java
@@ -1,6 +1,5 @@
 package com.lvu2code.springboot.demo.mycoolapp.rest;
 
-import com.lvu2code.springboot.demo.mycoolapp.entity.Student;
 import com.lvu2code.springboot.demo.mycoolapp.entity.StudentData;
 import jakarta.annotation.PostConstruct;
 import org.springframework.http.HttpStatus;
@@ -51,39 +50,5 @@ public class StudentRestController {
          return theStudents.get(studentId);
     }
 
-    // Add an exception handler using @ExceptionHandler
 
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc) {
-
-        // create a StudentErrorResponse
-
-        StudentErrorResponse error = new StudentErrorResponse();
-
-        error.setStatus(HttpStatus.NOT_FOUND.value());
-        error.setMessage(exc.getMessage());
-        error.setTimeStamp(System.currentTimeMillis());
-
-        // return ResponseEntity
-
-        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
-    }
-
-    // add another exception handler ... to catch any exception (catch all)
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException(Exception exc) {
-
-        // create a StudentErrorResponse
-
-        StudentErrorResponse error = new StudentErrorResponse();
-
-        error.setStatus(HttpStatus.BAD_REQUEST.value());
-        error.setMessage(exc.getMessage());
-        error.setTimeStamp(System.currentTimeMillis());
-
-        // return ResponseEntity
-
-        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
-
-    }
 }
